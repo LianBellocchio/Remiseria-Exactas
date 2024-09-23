62f489ea24f40676a5e704a7d8340d4e5edb66c4
Maite Kuhn <maikuhn@live.com>
2024-09-23 17:36:41 -0300

    nuevo datos agregados a Historial

--- backend/Conductores/Historial-viajes-del-dia-exactas.php
+++ backend/Conductores/Historial-viajes-del-dia-exactas.php
@@ -1,1 +1,12 @@
 Viajes del Día
+FECHA 23/09/2024
+    
+            Chacabuco 739 a Pueyrredon 28 - $2850
+        
+            Av Rivadavia 678 a Sarmiento 1678 - $4900
+        
+            Pinto 1600 a 14 de Julio 195 - $3200"
+
+            Unicen Campus Universitario a Gral Paz 1300 - $8400
+       
+            Alem 403 a San Lorenzo 1265 - $2700
