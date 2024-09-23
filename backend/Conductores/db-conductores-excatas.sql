e1943aab02588ab8a5016a36e17ffcf8046c1768
Maite Kuhn <maikuhn@live.com>
2024-09-23 16:56:50 -0300

    Datos agregados a DB Conductores y a Historial de viajes

--- backend/Conductores/db-conductores-excatas.sql
+++ backend/Conductores/db-conductores-excatas.sql
@@ -1,1 +1,7 @@
 Base de Datos - Conductores Remiseria Exactas
+"Conductor 1 - Federico Poupeau Crego - Patente: AH 768 PG"
+"Conductor 2 - Maite Kuhn - Patente: BT 196 AA"
+"Conductor 3 - Maria Paz Molfese - Patente:  ZHW 586"
+"Conductor 4 - Josefina Socobehere - Patente: NOL 365"
+"Conductor 5 - Pilar Costa Bauer - Patente: RPS 257"
+"Conductor 6 - Giuliano Bellocchio - Patente: BG 389 NO"
