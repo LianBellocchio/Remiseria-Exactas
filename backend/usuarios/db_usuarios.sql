8b20c98de46eee1788ded28371e41d5891aa7f62
josefina <josefinasocobehere@gmail.com>
2024-09-23 16:56:19 -0300

    Datos de Usuarios agregado a los archivos correspondientes

--- backend/usuarios/db_usuarios.sql
+++ backend/usuarios/db_usuarios.sql
@@ -1,1 +1,6 @@
-Base de datos de Usuarios
+Base de datos de Usuarios  
+
+Usuario 1 = "Usuario: Josefina Socobehere" "Contraseña:1234567890" 
+Usuario 2 = "Usuario: Maite Kunh" "Contraseña:098765432" 
+Usuario 3 = "Usuario: Lorena Garcia" "Contraseña: orense123"
+Usuario 4 = "Usuario: Xiomara Garcia" "Contraseña:tandil4567"  
--- backend/usuarios/historial-Usuario.php
+++ backend/usuarios/historial-Usuario.php
@@ -1,1 +1,15 @@
+
 Historial de viaje de usuario
+"Usuario 1 - Josefina Socobehere
+    Chacabuco 739 a Pueyrredon 28 - $2850
+
+"Usuario 2 - Maite Kunh"
+    Av Rivadavia 678 a Sarmiento 1678 - $4900
+
+"Usuario 3 - Lorena Garcia"
+    Pinto 1600 a 14 de Julio 195 - $3200
+
+"Usuario 4 - Xiomara Garcia"
+    Unicien Campus Universitario a Gral Paz 1300 - $8400
+        
+
